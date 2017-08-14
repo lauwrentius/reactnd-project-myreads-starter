380713a29261695b6adb0b4a3ae683127890182c
Init Built <lauwrentius.m@vmgstudio520.com>
2017-08-14 15:31:48 -0700

    minor Tweak

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Link, Route, Router } from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
 import PropTypes from 'prop-types'
+
 
 class SearchBook extends Component {
   static propTypes = {
-    onBookSearch:  PropTypes.func.isRequired
+    onBookSearch: PropTypes.func.isRequired
   }
+  state = {
+    searchVal: ""
+  }
+  handleChange = (evt) => {
+    this.setState({searchVal: evt.target.value});
+  }
+  onSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("SUBMIT")
+    this.props.onBookSearch(this.state.searchVal)
+  }
+  history = createBrowserHistory()
   render() {
     return (
       <div className="search-books-bar">
         <Link to='/' className="close-search">Close</Link>
-        <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author"/>
-        </div>
+        <form onSubmit={this.onSubmit}>
+          <div className="search-books-input-wrapper">
+            <input type="text"
+              value={this.state.searchVal}
+              onChange={this.handleChange}
+              placeholder="Search by title or author"/>
+          </div>
+        </form>
       </div>
     )
   }
